a8c656b9c799adf4ef0bf4e6e4b84d9c8341ffe1
Alcio Cavalheiro <alciocavalheiro@gmail.com>
2026-06-30 05:23:06 -0400

    Configura chaves do Supabase

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Preencha com os dados do SEU projeto Supabase
 // (Supabase Dashboard > Project Settings > API)
-const SUPABASE_URL = 'COLE_AQUI_A_PROJECT_URL';
-const SUPABASE_ANON_KEY = 'COLE_AQUI_A_ANON_PUBLIC_KEY';
+const SUPABASE_URL = 'https://obrrmokecoazctprargp.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9icnJtb2tlY29hemN0cHJhcmdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3ODU1NDcsImV4cCI6MjA5ODM2MTU0N30.zrf6YO7-t6uwOY3Ob5G10kW6OqkpSurqoBdMQqsov2o';
